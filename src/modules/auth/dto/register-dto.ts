1a6aec7e360f37beb97f23d6d88266219f6245eb
Mostafa-Elsaeed <mostafaelsaeed1544@gmail.com>
2026-02-01 15:46:52 +0200

    Work Done: - fix role-enum import

--- src/modules/auth/dto/register-dto.ts
+++ src/modules/auth/dto/register-dto.ts
@@ -1,18 +1,17 @@
 import { IsEmail, IsNotEmpty, IsString } from "class-validator";
-import { RoleEnum } from "src/modules/users/role.enum";
-
+import { RoleEnum } from "../../users/role.enum";
 
 export class RegisterDto {
-    @IsEmail()
-    @IsNotEmpty()
-    email: string;
-    @IsString()
-    @IsNotEmpty()
-    name: string;
-    @IsString()
-    @IsNotEmpty()
-    password: string;
-    @IsString()
-    @IsNotEmpty()
-    role: RoleEnum;
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+  @IsString()
+  @IsNotEmpty()
+  role: RoleEnum;
 }
