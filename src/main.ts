512ef95d4dcc3cefe39b4c2cac57ffc4b18b51d0
Mostafa-Elsaeed <mostafaelsaeed1544@gmail.com>
2026-02-04 11:52:19 +0200

    Work Done: - remove docker files

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { AppModule } from "./modules/app.module";
 import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   const port = process.env.PORT || 3000;
   console.log(port);
